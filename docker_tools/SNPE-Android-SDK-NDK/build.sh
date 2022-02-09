89e94b31343afa2e0f3ad35d6bd1dc19b677b264
Ming I Sun <89583471+MingSun1960@users.noreply.github.com>
2022-02-08 21:28:57 -0500

    Delete docker_tools directory accidentally added

--- docker_tools/SNPE-Android-SDK-NDK/build.sh
+++
@@ -1,6 +0,0 @@
-mkdir -p studio-data/profile/AndroidStudio4.2.2.0 || exit
-mkdir -p studio-data/Android || exit
-mkdir -p studio-data/profile/.android || exit
-mkdir -p studio-data/profile/.java || exit
-mkdir -p studio-data/profile/.gradle || exit
-docker build -t snpe-build-004 . || exit
--- docker_tools/SNPE-Android-SDK-NDK/utils/clean_docker.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-docker volume rm $(docker volume ls -qf dangling=true)
-docker rmi $(docker images --filter "dangling=true" -q --no-trunc)
-docker rm $(docker ps -qa --no-trunc --filter "status=exited"
